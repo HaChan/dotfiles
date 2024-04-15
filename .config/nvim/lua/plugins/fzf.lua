3f21dd7cf5e2008fb3d488aba522f4547e0f6d42
HaChan <hades.24791@gmail.com>
2024-04-16 00:52:16 +0700

    Add fuzzy find by custom directory in nvim

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -7,6 +7,13 @@ return {
 
     vim.keymap.set('n', '<leader>f?', require('fzf-lua').oldfiles, { desc = '[?] Find recently opened files' })
     vim.keymap.set('n', '<leader>ff', require('fzf-lua').files, { desc = '[f] Find recently opened files' })
+    vim.keymap.set('n', '<leader>fd', function()
+      vim.ui.input({prompt = 'Enter directory path: ', completion = 'dir'},
+        function (input)
+          require('fzf-lua').files({ cwd = input })
+        end
+      )
+    end , { desc = '[f] Find recently opened files' })
     vim.keymap.set('n', '<leader>fb', require('fzf-lua').buffers, { desc = '[b] Find existing buffers' })
     vim.keymap.set('n', '<leader>f/', require('fzf-lua').grep_curbuf, { desc = '[/] Fuzzily search in current buffer' })
   end
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -90,11 +90,33 @@ return {
     vim.keymap.set('n', '<leader>ss', require('telescope.builtin').builtin, { desc = '[S]earch [S]elect Telescope' })
     vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
     vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+    vim.keymap.set('n', '<leader>sc', function ()
+      require('telescope.builtin').find_files({
+        cwd = vim.fn.expand('%:h'),
+      })
+    end, { desc = "[S]earch [F]iles in current file's path" })
+    vim.keymap.set('n', '<leader>sd', function()
+      vim.ui.input(
+        {
+          prompt = 'Enter directory path: ',
+          completion = 'dir',
+        },
+        function (input)
+          require('telescope.builtin').find_files({ cwd = input })
+        end
+      )
+      -- local directory = vim.fn.input('Enter directory: ', '', 'file')
+      -- if directory then
+      --   require('telescope.builtin').find_files({
+      --     cwd = directory,
+      --   })
+      -- end
+    end, { desc = 'Find files in user-specified directory' })
     vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
     vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
     vim.keymap.set('n', '<leader>sG', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by [G]rep on Git Root' })
-    vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+    vim.keymap.set('n', '<leader>sD', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
   end
 }
